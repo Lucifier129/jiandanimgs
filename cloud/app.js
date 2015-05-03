621abf3b10c7323ee94e8265f584d774e62edb6d
Jade <guyingjie129@163.com>
2015-05-03 20:05:27 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -12,12 +12,13 @@ app.set('views', path.join(__dirname, 'views'))   // 设置模板目录
 app.set('view engine', 'hbs')    // 设置 template 引擎
 
 
-app.use(favicon('public/favicon.ico'))
+//app.use(favicon('public/favicon.ico'))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static('public'))
+app.use(express.static('static'))
 
 app.get('/', function(req, res) {
   res.send('Jade Gu')
