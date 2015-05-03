b9c4bcee75963dfed0fcdb4544c078f3397d7bb2
Jade <guyingjie129@163.com>
2015-05-03 15:55:47 +0800

    change

--- cloud/app.js
+++ cloud/app.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var path = require('path')
-var imgs = require('cloud/routes/imgs')
+var imgs = require(path.join(__dirname, 'routes/imgs'))
 var app = express()
+var bodyParser = require('body-parser')
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'hbs');    // 设置 template 引擎
-app.use(express.bodyParser());    // 读取请求 body 的中间件
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
   res.send('Jade Gu')
